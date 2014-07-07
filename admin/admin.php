27efd74d6dabb0584c418b5c7cbdf4ca1fff2f8e
chrisull <cullman@velti.com>
2014-07-07 15:27:13 +0100

    Rename user and admin pages

--- admin/admin.php
+++ admin/admin.php
@@ -1,24 +1,15 @@
 <?php 
 require_once('authenticate.php'); 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
   
 /* Db Details */
 require_once('../includes/db_config.php');
 
 /* Query SQL Server for selecting data. */
-$tsql = "select * FROM user where role_id=1";
-$stmt = mysql_query($tsql);
-if(!$stmt)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-?>
-<?php include '../includes/header.php' ?>
-
-      <a class="btn btn-default" href="new-admin.php" role="button">New admin</a>
-
+$select_user_sql = "select * FROM user where role_id=1";
+$select_user_result = mysql_query($select_user_sql);
+if(!$select_user_result) { die("Query failed: ". mysql_error()); }
+include '../includes/header.php' ?>
+<a class="btn btn-default" href="new-admin.php" role="button">New admin</a>
       <!-- table of articles -->
       <table class="table table-hover">
         <thead>
@@ -31,31 +22,24 @@ if(!$stmt)
           </tr>
         </thead>
         <tbody>
-          <?php  while($row = mysql_fetch_array($stmt)) { ?>
+          <?php  while($select_user_row = mysql_fetch_array($select_user_result)) { ?>
           <tr>
-            <td><a href=""><?php echo $row['full_name']; ?></a></td>
-            <td><a href=""><?php echo $row['email']; ?></a></td>
-                 <td><?php if (isset($row['user_image'])) { ?><img width=50 src="../uploads/<?php echo $row['user_image']; ?>"/> <?php } else { ?> <img width=50 src="../uploads/blank.png" /> <?php } ?></td>
-            <td><?php echo $row['date_joined']; ?></td>
+            <td><a href=""><?php echo $select_user_row['full_name']; ?></a></td>
+            <td><a href="mailto:<?php echo $select_user_row['email']; ?>"><?php echo $select_user_row['email']; ?></a></td>
+                 <td><?php if (isset($select_user_row['user_image'])) { ?><img width=50 src="../uploads/<?php echo $select_user_row['user_image']; ?>"/> <?php } else { ?> <img width=50 src="../uploads/blank.png" /> <?php } ?></td>
+            <td><?php echo $select_user_row['date_joined']; ?></td>
             <td>
-<?php 
-/* Query SQL Server for total records data. */
-$tsql3 = "select Count(*) As ArticleComments FROM comments where user_id=".$row['user_id'] ;
-$stmt3 = mysql_query($tsql3);
-if(!$stmt3)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-$row3 = mysql_fetch_array($stmt3);
-$totalComments = $row3["ArticleComments"];
-echo $totalComments;
-?>
-
+            <?php 
+            /* Query SQL Server for total records data. */
+                     $select_totalcomments_sql = "select Count(*) As ArticleComments FROM comments where user_id=".$select_user_row['user_id'] ;
+            $select_totalcomments_result = mysql_query($select_totalcomments_sql);
+            if(!$select_totalcomments_result) {  die("Query failed: ". mysql_error()); }
+            $select_totalcomments_row = mysql_fetch_array($select_totalcomments_result);
+            $totalComments = $select_totalcomments_row["ArticleComments"];
+            echo $totalComments; ?>
             </td>
           </tr>
          <?php } ?>
         </tbody>
       </table>
-
 <?php include '../includes/footer.php' ?>
