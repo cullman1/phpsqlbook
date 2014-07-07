86e3995ec4a8b6bdae7fd4767dfd61ffd218d4c6
chrisull <cullman@velti.com>
2014-07-07 15:52:34 +0100

    Update categories

--- admin/edit-category.php
+++ admin/edit-category.php
@@ -1,66 +1,52 @@
 <?php 
 require_once('authenticate.php'); 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
   
 /* Db Details */
 require_once('../includes/db_config.php');
 
-/* Query SQL Server for inserting data. */
-$tsql = "select distinct category_template FROM 387732_phpbook1.category";
-$stmt = mysql_query($tsql);
-if(!$stmt)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
+/* Query SQL Server for selecting category template. */
+$select_categorytemplate_sql = "select distinct category_template FROM category";
+$select_categorytemplate_result = mysql_query($select_categorytemplate_sql);
+if(!$select_categorytemplate_result) { die("Query failed: ". mysql_error()); }
 
-$tsql2 = "select category_id, category_name, category_template FROM 387732_phpbook1.category where category_id=".$_REQUEST["categoryid"];
-$stmt2 = mysql_query($tsql2);
-if(!$stmt2)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-
-?>
-<?php include '../includes/headereditor2.php' ?>
+$select_category_sql = "select category_id, category_name, category_template FROM category where category_id=".$_REQUEST["categoryid"];
+$select_category_result = mysql_query($select_category_sql);
+if(!$select_category_result) {  die("Query failed: ". mysql_error()); }
+include '../includes/headereditor2.php' ?>
   <div id="body">
     <form id="form1" method="post" action="editcategory.php">
-       <?php while($row2 = mysql_fetch_array($stmt2)) { ?>
+       <?php while($select_category_row = mysql_fetch_array($select_category_result)) { ?>
       <div id="middlewide">
         <div id="leftcol">
           <h2>Edit Category</h2><br />
           <table>
             <tr>
-				      <td><span class="fieldheading">Category Name:</span></td>
-				      <td><input id="CategoryName" name="CategoryName" type="text" value="<?php echo $row2['category_name']; ?>" /></td> 
-			       </tr>
-            
-            <tr><td> </td><td>&nbsp; </td></tr>
+				 <td><span class="fieldheading">Category Name:</span></td>
+				 <td><input id="CategoryName" name="CategoryName" type="text" value="<?php echo $select_category_row['category_name']; ?>" /></td> 
+			</tr>
+            <tr><td></td><td>&nbsp; </td></tr>
             <tr>
               <td><span class="fieldheading">Category Template:</span></td>
-
               <td>
                 <select id="CategoryParent" name="CategoryParent">
-                 <?php while($row = mysql_fetch_array($stmt)) { ?>
-                <option <?php if( $row2['category_template'] == $row['category_template']) { echo "selected";} ?> ><?php  echo $row['category_template']; ?></option>
+                 <?php while($select_categorytemplate_row = mysql_fetch_array($select_categorytemplate_result)) { ?>
+                <option <?php if( $select_category_row['category_template'] == $select_categorytemplate_row['category_template']) { echo "selected";} ?> ><?php  echo $select_categorytemplate_row['category_template']; ?></option>
                   <?php } ?> 
                   </select>
               </td> 
              </tr>
               <tr><td> </td><td>&nbsp; </td></tr>
             <tr>
-				      <td></td>
-				      <td><input id="SaveButton" type="submit" name="submit" Value="Submit"  /></td>
-			       </tr> 
+		        <td></td>
+				<td><input id="SaveButton" type="submit" name="submit" Value="Submit"  /></td>
+			</tr> 
           </table>
           <input id="categoryid" name="categoryid" type="hidden" value="<?php echo $_REQUEST['categoryid']; ?>"/>
           <br />
           <br />
           <div id="Status" >
             <?php 
-	           if(isset($_GET['submitted']))
+	           if(isset($_REQUEST['submitted']))
 	           {
 		          echo "<span class='red'>Category successfully created</span>";
 	           }  
@@ -68,13 +54,11 @@ if(!$stmt2)
          </div>
       </div>
       <br />
-      <a id="Return2" href="../index.html">Return to Main Page</a>
+      <a id="Return2" href="../admin/categories.php">Return to Main Page</a>
       </div>
     </form>
     <?php } ?>
   <!--end content --> 
-  </div>
-
-  <div class="clear"></div>
-
+</div>
+<div class="clear"></div>
 <?php include '../includes/footereditor.php' ?>
--- admin/editcategory.php
+++ admin/editcategory.php
@@ -1,26 +1,14 @@
 <?php 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
-  
 /* Db Details */
 require_once('../includes/db_config.php');
 
 /* Query */
-$tsql = 'UPDATE 387732_phpbook1.category SET category_name= "' .$_REQUEST["CategoryName"].'", category_template="' .$_REQUEST["CategoryParent"]. '" where category_id='.$_REQUEST["categoryid"];
-$stmt = mysql_query($tsql);
-
-if(!$stmt)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
+$update_category_sql = 'UPDATE category SET category_name= "' .$_REQUEST["CategoryName"].'", category_template="' .$_REQUEST["CategoryParent"]. '" where category_id='.$_REQUEST["categoryid"];
+$update_category_result = mysql_query($update_category_sql);
+if(!$update_category_result) {  die("Query failed: ". mysql_error()); }
 else
-{
-
-	
+{	
 	/* Redirect to original page */
-    header('Location:../admin/categories.php');
-		
-	
+    header('Location:../admin/categories.php');	
 }
 ?>
