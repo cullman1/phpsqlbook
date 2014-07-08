4cd38908a593db8d18b512f9173500b747640387
cullman1 <cullman@velti.com>
2014-07-08 10:04:18 +0100

    Comments variables renamed

--- admin/comments.php
+++ admin/comments.php
@@ -1,25 +1,15 @@
 <?php
 require_once('authenticate.php'); 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
   
 /* Db Details */
 require_once('../includes/db_config.php');
 
-
 /* Query SQL Server for selecting data. */
-$tsql = "select * FROM comments JOIN user ON comments.user_id = user.user_id JOIN article ON article.article_id = comments.article_id";
-$stmt = mysql_query($tsql);
-if(!$stmt)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-
-?>
-<?php include '../includes/header.php' ?>
-
-      <!-- table of articles -->
+$select_comments_sql = "select * FROM comments JOIN user ON comments.user_id = user.user_id JOIN article ON article.article_id = comments.article_id";
+$select_comments_result = mysql_query($select_comments_sql);
+if(!$select_comments_result) {      die("Query failed: ". mysql_error()); }
+include '../includes/header.php' ?>
+<!-- table of articles -->
       <table class="table table-hover">
         <thead>
           <tr>
@@ -31,21 +21,20 @@ if(!$stmt)
         </thead>
         <tbody>
           <?php 
-          while($row = mysql_fetch_array($stmt)) { ?>
+          while($select_comments_row = mysql_fetch_array($select_comments_result)) { ?>
           <tr>
             <td>
-                 <?php if  ($row['role_id']==1) { ?>
+                 <?php if  ($select_comments_row['role_id']==1) { ?>
                 <a href="../admin/admin.php">
- <?php } else if  ($row['role_id']==2) { ?>
+ <?php } else if  ($select_comments_row['role_id']==2) { ?>
                          <a href="../admin/admin.php">
                 <?php } ?>
-                <?php echo $row['user_name']; ?></a></td>
-            <td><?php echo $row['comment']; ?></td>
-            <td><a href="../admin/editarticle2.php?article_id=<?php echo $row['article_id']; ?>"><?php echo $row['title']; ?></a></td>
+                <?php echo $select_comments_row['user_name']; ?></a></td>
+            <td><?php echo $select_comments_row['comment']; ?></td>
+            <td><a href="../admin/editarticle2.php?article_id=<?php echo $select_comments_row['article_id']; ?>"><?php echo $select_comments_row['title']; ?></a></td>
             <td><a href=""><span class="glyphicon glyphicon-remove"></span></a></td>
           </tr>
            <?php } ?>
         </tbody>
       </table>
-
 <?php include '../includes/footer.php' ?>
