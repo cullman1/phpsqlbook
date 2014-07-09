fb37ada9a4f08f0a73612412d1627005a231b6f6
chrisull <cullman@velti.com>
2014-07-09 11:55:05 +0100

    Add user refectored

--- admin/adduser.php
+++ admin/adduser.php
@@ -1,9 +1,9 @@
 <?php 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
-  
+/* Login check */
+require_once('authenticate.php'); 
+
 /* Include passwords and login details */
-require_once('../includes/loginvariables.php');
+require_once('../includes/dbconfig.php');
 
 if (empty( $_REQUEST['password']) || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName']) || empty($_REQUEST['emailAddress']) )
 {
@@ -20,81 +20,57 @@ if (empty( $_REQUEST['password']) || empty($_REQUEST['firstName']) || empty($_RE
 }
 else
 {
+    /* Hash password */
+    $passwordToken = sha1($preSalt . $_REQUEST['password'] . $afterSalt);
 
-/* Connect using MySql Authentication. */
-$conn = mysql_connect( $serverName, $userName, $password);
-if(!$conn)
-{
-        die("Unable to connect. Error: " . mysql_error());
-}
-  
-/* Select db */
-mysql_select_db($databaseName) or die ("Couldn't select db. Error:"  . mysql_error()); 
- 
-/* Hash password */
-
-$passwordToken = sha1($preSalt . $_REQUEST['password'] . $afterSalt);
-
-/* Query SQL Server for checking existing user. */
-$tsql1 = "SELECT * from user WHERE email = '".$_REQUEST['emailAddress']."'";
-
-
-$stmt1 = mysql_query($tsql1);
-if(!$stmt1)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-else
-{
-
-	if(mysql_num_rows($stmt1)>0)
-	{
-			/* Redirect to original page */
-  		$name = $_REQUEST['page'];
-  		if($name=="register")
-  		{
-			header('Location:../login/'.$name.'.php?submitted=false');
-  		}
-  		else
-  		{
-    		header('Location:new-'.$name.'.php?submitted=false');
-		}
-	}	
-	else
-	{
-		/* Query SQL Server for inserting data. */
-
-    $name="";
-    if(isset($_FILES['uploader']))
+    /* Query SQL Server for checking existing user. */
+    $select_user_sql = "SELECT * from user WHERE email = '".$_REQUEST['emailAddress']."'";
+    $select_user_result = mysql_query($select_user_sql);
+    if(!$select_user_result)
+    {  die("Query failed: ". mysql_error()); }
+    else
     {
-        $name = $_FILES["uploader"]["name"];
-        move_uploaded_file($_FILES['uploader']['tmp_name'], $folder);
+	    if(mysql_num_rows($select_user_result)>0)
+	    {
+			/* Redirect to original page */
+  		    $name = $_REQUEST['page'];
+  		    if($name=="register")
+  		    {
+			    header('Location:../login/'.$name.'.php?submitted=false');
+  		    }
+  		    else
+  		    {
+    		    header('Location:new-'.$name.'.php?submitted=false');
+		    }
+	    }	
+	    else
+	    {
+		    /* Query SQL Server for inserting data. */
+            $name="";
+            if(isset($_FILES['uploader']))
+            {
+                $name = $_FILES["uploader"]["name"];
+                move_uploaded_file($_FILES['uploader']['tmp_name'], $folder);
+            }
+
+            /* Query SQL Server for inserting new user. */
+		    $insert_user_sql = "INSERT INTO user (full_name, password, email, role_id, date_joined, user_image) VALUES ('".$_REQUEST['firstName']." ".$_REQUEST['lastName']."', '".$passwordToken."', '".$_REQUEST['emailAddress']."','".$_REQUEST['Role']."', '". date("Y-m-d H:i:s") ."', '". $name ."')";
+		    $insert_user_result = mysql_query($insert_user_sql);
+		    if(!$insert_user_result) {  die("Query failed: ". mysql_error()); }
+		    else
+		    {
+  			    /* Redirect to original page */
+  			    $name = $_REQUEST['page'];
+  			    if($name=="register")
+  			    {
+  				    header('Location:../login/'.$name.'.php?submitted=true');
+  			    }
+  			    else
+  			    {
+    			    header('Location:'.$name.'.php?submitted=true');
+			    }
+	        }
+	    }
     }
-
-    /* Query SQL Server for inserting data. */
-		$tsql = "INSERT INTO user (full_name, password, email, role_id, date_joined, user_image) VALUES ('".$_REQUEST['firstName']." ".$_REQUEST['lastName']."', '".$passwordToken."', '".$_REQUEST['emailAddress']."','".$_REQUEST['Role']."', '". date("Y-m-d H:i:s") ."', '". $name ."')";
-		$stmt = mysql_query($tsql);
-		if(!$stmt)
-		{  
-    		/* Error Message */
-    		die("Query failed: ". mysql_error());
-		}
-		else
-		{
-  			/* Redirect to original page */
-  			$name = $_REQUEST['page'];
-  			if($name=="register")
-  			{
-  				header('Location:../login/'.$name.'.php?submitted=true');
-  			}
-  			else
-  			{
-    			header('Location:'.$name.'.php?submitted=true');
-			}
-	}
-	}
-
-}
 }
 ?>
