6663f3ee7598a5ed0e68f5321940c45cff6836c3
chrisull <cullman@velti.com>
2014-07-09 11:42:49 +0100

    Removal of addcategory page and merging into one and removal of old pages

--- admin/categories.php
+++ admin/categories.php
@@ -5,7 +5,7 @@ require_once('authenticate.php');
 require_once('../includes/db_config.php');
 
 /* Query SQL Server for selecting categories by group. */
-$select_categoriesbygroup_sql = "select category_name, category_template, category.category_id, count(*)  as items FROM category JOIN article ON article.category_id = category.category_id group by category_name having count(*) > 0";
+$select_categoriesbygroup_sql = "select category_name, category_template, category.category_id, count(article_id) as items FROM category Left outer JOIN article ON article.category_id = category.category_id  group by category_name "; 
 $select_categoriesbygroup_result = mysql_query($select_categoriesbygroup_sql);
 if(!$select_categoriesbygroup_result)
 {  
--- admin/new-category.php
+++ admin/new-category.php
@@ -1,61 +1,63 @@
 <?php 
 require_once('authenticate.php'); 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
   
 /* Db Details */
 require_once('../includes/db_config.php');
 
 /* Query SQL Server for inserting data. */
-$tsql = "select distinct category_template FROM 387732_phpbook1.category";
-$stmt = mysql_query($tsql);
-if(!$stmt)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
+$select_category_sql = "select distinct category_template FROM category";
+$select_category_result = mysql_query($select_category_sql);
+if(!$select_category_result) { die("Query failed: ". mysql_error()); }
 
-?>
-<?php include '../includes/headereditor2.php' ?>
+if (isset($_REQUEST["Submitted"]))
+{
+    /* Query SQL Server for inserting a new category. */
+    $insert_category_sql = "INSERT INTO category (category_name, category_template) VALUES ('".$_REQUEST['CategoryName']."', '".$_REQUEST['CategoryParent']."')";
+    $insert_category_result = mysql_query($insert_category_sql);
+    if(!$insert_category_result) { die("Query failed: ". mysql_error()); }
+    else
+    {
+        /* Redirect to original page */
+        header('Location:../admin/categories.php');
+    }   
+}
+include '../includes/headereditor2.php' ?>
   <div id="body">
-    <form id="form1" method="post" action="addcategory.php">
+    <form id="form1" method="post" action="new-category.php">
       <div id="middlewide">
         <div id="leftcol">
           <h2>Add New Category</h2><br />
           <table>
             <tr>
-				      <td><span class="fieldheading">Category Name:</span></td>
-				      <td><input id="CategoryName" name="CategoryName" type="text" /></td> 
-			       </tr>
-            
-            <tr><td> </td><td>&nbsp; </td></tr>
+				   <td><span class="fieldheading">Category Name:</span></td>
+				   <td><input id="CategoryName" name="CategoryName" type="text" /></td> 
+			</tr>
+            <tr>
+                <td> </td><td>&nbsp; </td>
+            </tr>
             <tr>
               <td><span class="fieldheading">Category Template:</span></td>
-
               <td>
                 <select id="CategoryParent" name="CategoryParent">
-                 <?php while($row = mysql_fetch_array($stmt)) { ?>
-                <option><?php  echo $row['category_template']; ?></option>
-                  <?php } ?> 
-                  </select>
+                    <?php while($select_category_row = mysql_fetch_array($select_category_result)) { ?>
+                    <option><?php  echo $select_category_row['category_template']; ?></option>
+                    <?php } ?> 
+                </select>
               </td> 
-             </tr>
-              <tr><td> </td><td>&nbsp; </td></tr>
+            </tr>
             <tr>
-				      <td></td>
-				      <td><input id="SaveButton" type="submit" name="submit" Value="Submit"  /></td>
-			       </tr> 
+                <td> </td><td>&nbsp; </td>
+            </tr>
+            <tr>
+				<td></td>
+                <td>
+                    <input id="SaveButton" type="submit" name="submit" Value="Submit"  />
+                    <input id="Submitted" name="Submitted" type="hidden" value="true" />
+			    </td>
+			</tr> 
           </table>
           <br />
           <br />
-          <div id="Status" >
-            <?php 
-	           if(isset($_GET['submitted']))
-	           {
-		          echo "<span class='red'>Category successfully created</span>";
-	           }  
-	         ?>
-         </div>
       </div>
       <br />
       <a id="Return2" href="../index.html">Return to Main Page</a>
@@ -63,7 +65,5 @@ if(!$stmt)
     </form>
   <!--end content --> 
   </div>
-
-  <div class="clear"></div>
-
+<div class="clear"></div>
 <?php include '../includes/footereditor.php' ?>
