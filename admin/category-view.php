4ef5dcd3aabefc8f70f7b4523ef8690ef97aac59
chrisull <cullman@velti.com>
2014-07-09 12:09:14 +0100

    Refactored author and category view pages

--- admin/category-view.php
+++ admin/category-view.php
@@ -1,49 +1,33 @@
 <?php
 require_once('authenticate.php'); 
-error_reporting(E_ALL | E_WARNING | E_NOTICE);
-ini_set('display_errors', TRUE);
   
 /* Db Details */
 require_once('../includes/db_config.php');
 
-/* Query SQL Server for selecting data. */
-$tsql = "select article_id, title, content, category_name, category.category_id, user_name, date_posted, role_id FROM article JOIN user ON article.user_id = user.user_id JOIN category ON article.category_id = category.category_id where category.category_id = ".$_REQUEST["categoryid"]." order by article_id";
-$stmt = mysql_query($tsql);
-if(!$stmt)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-
+/* Query SQL Server for selecting article. */
+$select_article_sql = "select article_id, title, content, category_name, category.category_id, user_name, date_posted, role_id FROM article JOIN user ON article.user_id = user.user_id JOIN category ON article.category_id = category.category_id where category.category_id = ".$_REQUEST["categoryid"]." order by article_id";
+$select_article_result = mysql_query($select_article_sql);
+if(!$select_article_result) { die("Query failed: ". mysql_error()); }
 
 /* Query SQL Server for total records data. */
-$tsql2 = "select Count(*) As TotalRecords FROM article";
-$stmt2 = mysql_query($tsql2);
-if(!$stmt2)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-$row2 = mysql_fetch_array($stmt2);
-$totalRecords = $row2["TotalRecords"];
+$select_totalrecords_sql = "select Count(*) As TotalRecords FROM article";
+$select_totalrecords_result = mysql_query($select_totalrecords_sql);
+if(!$select_totalrecords_result) {      die("Query failed: ". mysql_error()); }
+$select_totalrecords_row = mysql_fetch_array($select_totalrecords_result);
+$totalRecords = $select_totalrecords_row["TotalRecords"];
 
-/* Query SQL Server for total records data. */
-$tsql3 = "select category_name from category where category_id= ".$_REQUEST["categoryid"];
-$stmt3 = mysql_query($tsql3);
-if(!$stmt3)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-$row3 = mysql_fetch_array($stmt3);
-$catName = $row3["category_name"];
-?>
-<?php include '../includes/header.php' ?>
-
-      <div>Category:
-      <button type="button" class="btn btn-default"><?php echo $catName; ?></button></div>
-      <!-- table of articles -->
-      <table class="table table-hover">
+/* Query SQL Server for category. */
+$select_category_sql = "select category_name from category where category_id= ".$_REQUEST["categoryid"];
+$select_category_result = mysql_query($select_category_sql);
+if(!$select_category_result) {   die("Query failed: ". mysql_error()); }
+$select_category_row = mysql_fetch_array($select_category_result);
+$catName = $select_category_row["category_name"];
+include '../includes/header.php' ?>
+<div>Category:
+      <button type="button" class="btn btn-default"><?php echo $catName; ?></button>
+</div>
+<!-- table of articles -->
+<table class="table table-hover">
         <thead>
           <tr>
             <th>Title</th>
@@ -63,39 +47,31 @@ $catName = $row3["category_name"];
             $endPage = ($_REQUEST["page"] * $recordsPerPage);
           }
           $count=1;
-          while($row = mysql_fetch_array($stmt)) { 
- //echo "<span>Count ".$count." Start Page ".$startPage. " End Page " .$endPage. " Records Per Page " . $recordsPerPage." </span><br/>";
-          if (($count>= $startPage) && ($count <= $endPage))
-          { ?>
-          <tr>
-            <td><a href="EditArticle2.php?article_id=<?php echo $row['article_id'];?>"><?php echo $row['title']; ?></a></td>
-            <td><a href="<?php if ($row['role_id']==1) { echo 'admins.php';} else { echo 'users.php';} ?>"><?php echo $row['user_name']; ?></a></td>
-            <td>Published: <?php echo $row['date_posted']; ?></td>
+          while($select_article_row = mysql_fetch_array($select_article_result)) 
+          { 
+            if (($count>= $startPage) && ($count <= $endPage))
+            { ?>
+            <tr>
+            <td><a href="EditArticle2.php?article_id=<?php echo $select_article_row['article_id'];?>"><?php echo $select_article_row['title']; ?></a></td>
+            <td><a href="<?php if ($select_article_row['role_id']==1) { echo 'admins.php';} else { echo 'users.php';} ?>"><?php echo $select_article_row['user_name']; ?></a></td>
+            <td>Published: <?php echo $select_article_row['date_posted']; ?></td>
             <td>
-<?php 
-/* Query SQL Server for total records data. */
-$tsql3 = "select Count(*) As ArticleComments FROM comments where article_id=".$row['article_id'] ;
-$stmt3 = mysql_query($tsql3);
-if(!$stmt3)
-{  
-    /* Error Message */
-    die("Query failed: ". mysql_error());
-}
-$row3 = mysql_fetch_array($stmt3);
-$totalComments = $row3["ArticleComments"];
-echo $totalComments;
-?>
-
+            <?php 
+                /* Query SQL Server for comments count. */
+                $select_comments_sql = "select Count(*) As ArticleComments FROM comments where article_id=".$select_article_row['article_id'] ;
+                $select_comments_result = mysql_query($select_comments_sql);
+                if(!$select_comments_result) {      die("Query failed: ". mysql_error()); }
+                $select_comments_row = mysql_fetch_array($select_comments_result);
+                $totalComments = $select_comments_row["ArticleComments"];
+                echo $totalComments;
+            ?>
             </td>
-            <td><a onclick="javascript:return confirm(&#39;Are you sure you want to delete this item 652&#39;);" id="delete1" href="deletedata.php?article_id=<?php echo $row['article_id'];?>".><span class="glyphicon glyphicon-remove"></span></a></td>
+            <td><a onclick="javascript:return confirm(&#39;Are you sure you want to delete this item 652&#39;);" id="delete1" href="deletedata.php?article_id=<?php echo $select_article_row['article_id'];?>".><span class="glyphicon glyphicon-remove"></span></a></td>
          <?php 
         }
          $count = $count+1;
        } ?>
-         </tr>
-        </tbody>
-      </table>
-
-
-
+      </tr>
+   </tbody>
+</table>
 <?php include '../includes/footer.php' ?>
