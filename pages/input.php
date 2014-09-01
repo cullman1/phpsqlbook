149aa8408ce70c319f62a9c61052cb76f8dc6e5c
chrisull <cullman@velti.com>
2014-09-01 13:27:19 +0100

    Chapter 2 example files

---
+++ pages/input.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+<form method=post action="../pages/query.php">
+<label>Select a user</label>
+<select id="user_value" name="user_value">
+<option value=1>Pablo</option>
+<option value=2>Jackson</option>
+<option value=3>Vincent</option>
+</select>
+    <br />
+    <br />
+<input type=submit value=submit />
+</form>
+</body>
+</html>
