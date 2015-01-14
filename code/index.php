c6a81d37b9ab45cc0e4a721b3f82cbbbf458b68a
chrisull <cullman@velti.com>
2015-01-14 16:37:49 +0000

    Example chapters

--- code/index.php
+++ code/index.php
@@ -5,31 +5,41 @@ include '../includes/header-chapters.php' ?>
   <div class="wholeform2">
       <div class="col-md-4" style="padding-left:23%;"><h1>Code examples</h1><br /></div>
         
-        <a class="linked" href="/code/chapter1/chapter1.php"><div class="col-md-4 boxy1">
+        <a class="linked" href="/code/chapter1/chapter1.php">
+            <div class="col-md-4 boxy1">
          <h3>Chapter 1</h3>
        
              <div>Introduction to Server-side programming</div>
         </div></a>
       
-         <a class="linked" href="/code/chapter2/chapter2.php">
+         <a class="linked" href="/code/chapter1b/chapter1b.php">
           <div class="col-md-4 boxy2">
          <h3>Chapter 2</h3>
                
-             <div>Getting data from a database into your templates</div>
+             <div>PHP constructs</div>
         </div>
           </a>
       <br />
 
-      <a class="linked" href="/code/chapter3/chapter3.php">
-        <div class="col-md-4 boxy1">
+         <a class="linked" href="/code/chapter2/chapter2.php">
+          <div class="col-md-4 boxy1">
          <h3>Chapter 3</h3>
+               
+             <div>Getting data from a database into your templates</div>
+        </div>
+          </a>
+   
+
+      <a class="linked" href="/code/chapter3/chapter3.php">
+        <div class="col-md-4 boxy2">
+         <h3>Chapter 4</h3>
              
              <div>Server side Processing of data</div>
        </div></a>
-
+         <br />
       <a class="linked" href="/code/chapter4/chapter4.php">
-        <div class="col-md-4 boxy2">
-         <h3>Chapter 4</h3>
+        <div class="col-md-4 boxy1">
+         <h3>Chapter 5</h3>
                
              <div>Updating the database</div>
             
@@ -37,8 +47,8 @@ include '../includes/header-chapters.php' ?>
           </a>
             <br />
             <a class="linked" href="/code/chapter5/chapter5.php">
-        <div  class="col-md-4 boxy1">
-         <h3>Chapter 5</h3>
+        <div  class="col-md-4 boxy2">
+         <h3>Chapter 6</h3>
                
              <div>Media and Files</div>
             
@@ -47,39 +57,48 @@ include '../includes/header-chapters.php' ?>
   
 
    <a class="linked" href="/code/chapter6/chapter6.php">
-        <div class="col-md-4 boxy2">
-         <h3>Chapter 6</h3>
+        <div class="col-md-4 boxy1">
+         <h3>Chapter 7</h3>
                
              <div>Membership</div>
             
        </div>
           </a>
       <br />
-          <a class="linked" href="/code/chapter7/chapter7.php"><div class="col-md-4 boxy1">
-         <h3>Chapter 7</h3>
+          <a class="linked" href="/code/chapter7/chapter7.php"><div class="col-md-4 boxy2">
+         <h3>Chapter 8</h3>
        
              <div>Application Design</div>
         </div></a>
       
          <a class="linked" href="/code/chapter8/chapter8.php">
-          <div class="col-md-4 boxy2">
-         <h3>Chapter 8</h3>
+          <div class="col-md-4 boxy1">
+         <h3>Chapter 9</h3>
                
              <div>Objects </div>
         </div>
           </a>
       <br />
-        <a class="linked" href="/code/chapter9/chapter9.php"><div class="col-md-4 boxy1">
-         <h3>Chapter 9</h3>
+        <a class="linked" href="/code/chapter9/chapter9.php"><div class="col-md-4 boxy2">
+         <h3>Chapter 10</h3>
        
              <div>APIs</div>
         </div></a>
       
          <a class="linked" href="/code/chapter10/chapter10.php">
-          <div class="col-md-4 boxy2">
-         <h3>Chapter 10</h3>
+          <div class="col-md-4 boxy1">
+         <h3>Chapter 11</h3>
                
              <div>Error Handling and Troubleshooting </div>
+        </div>
+          </a>
+ 
+
+       <a class="linked" href="/code/chapter12/chapter12.php">
+          <div class="col-md-4 boxy2">
+         <h3>Extras</h3>
+               
+             <div>Extras </div>
         </div>
           </a>
       <br />
