2ec8a0d61706447f6d3aea8d042b1474fe93c7b6
chrisull <cullman@velti.com>
2015-01-26 16:46:36 +0000

    Facebook SDK

--- pages/main.php
+++ pages/main.php
@@ -7,6 +7,21 @@
     <div class="pad">
         <?php include 'cms-viewer.php' ?>
     </div>
+    <div class="side">
+        <?php
+
+include_once '../Facebook/facebook.php';
+$config = array('appId' => '464651713667817', 'secret' => 'a8f67bca9e608806baf6a2fae8b53d5b', 'cookie' => true);
+$facebook = new Facebook($config);
+
+//Get User ID
+include '../Facebook/FacebookSidebar.php';
+
+ ?>
+
+    </div>
+
+
  </div>
 <?php include('../includes/pagination.php'); ?>
 <?php include '../includes/footer-site.php' ?>
