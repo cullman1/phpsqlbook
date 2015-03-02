1572e06f2db7236938dcb4c7ccfa5512cad86832
chrisull <cullman@velti.com>
2015-03-02 16:40:26 +0000

    Updated slightly

--- code/includes/footerclass.php
+++ code/includes/footerclass.php
@@ -4,7 +4,9 @@ class Footer {
     public function getLinks() {  
         require_once ("../includes/links.php");
     }
-    
+    public function getFooterContent() {  
+        require_once ("../includes/footer-site.php");
+    }
 }
 
 ?>
--- code/includes/headerclass.php
+++ code/includes/headerclass.php
@@ -4,6 +4,9 @@ class Header {
     public function getTemplatePath() {  
         return "../includes/style.css";
     }
+    public function getHeaderContent() {  
+        require_once "../includes/header.php";
+    }
     
 }
 
