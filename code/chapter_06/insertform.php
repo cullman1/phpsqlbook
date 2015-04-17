f13268653f215e346a7b64058e2d490f6bef9567
cullman1 <cullman@velti.com>
2015-04-17 18:45:21 +0100

    More example rewrites

--- code/chapter_06/insertform.php
+++ code/chapter_06/insertform.php
@@ -1,24 +1,15 @@
 <?php  include '../includes/header-register.php'; ?>
- <form id="form1" method="post" action="insertdata.php">
-  <div class="wholeform">
-    <div class="col-md-4"><h1>Please register:</h1></div>
-      <div class="form-group">
+ <form id="form1" class="indent" method="post" action="insertdata.php">
+    <div class="col-md-4"><h1>Please register:</h1>   
         <label for="email">Email address</label>
         <input type="email" class="form-control" name="email" placeholder="Enter email">
-       </div>
-       <div class="form-group">
         <label for="firstName">First name</label>
         <input type="text" class="form-control" name="firstName" placeholder="First name">
-       </div>
-       <div class="form-group">
-         <label for="lastName">Last name</label>
-         <input type="text" class="form-control" name="lastName" placeholder="Last name">
-        </div>
-        <div class="form-group">
-          <label for="pwd">Password</label>
-          <input type="password" class="form-control" name="pwd" placeholder="Password">
-         </div>      
+        <label for="lastName">Last name</label>
+        <input type="text" class="form-control" name="lastName" placeholder="Last name">
+        <label for="pwd">Password</label>
+        <input type="password" class="form-control" name="pwd" placeholder="Password">
         <button type="submit" class="btn btn-default">Register</button>
-   </div>
+     </div>
 </form>
 <?php require_once('../includes/footer-site.php'); ?>
---
+++ code/chapter_06/updateform.php
@@ -0,0 +1,26 @@
+<?php require_once('../includes/db_config.php');
+include '../includes/header-register.php'; 
+$sel_user_sql = "select * FROM Users where user_id=1001";
+$sel_user_set = $dbHost->prepare($sel_user_sql);
+$sel_user_set->execute();
+$sel_user_set->setFetchMode(PDO::FETCH_ASSOC); ?>
+<form id="form1" class="indent" method="post" action="submit-user.php">
+   <?php while($sel_user_row = $sel_user_set->fetch()) { ?>
+      <div id="col-md-4">
+         <h2>Edit User</h2>
+         <label class="fieldheading">User Name:</label>
+         <input name="uame" type="text" value="<?php echo $sel_user_row['full_name'];?>" />
+         <label  class="fieldheading">User Email:</label>
+         <input name="email" type="text" value="<?php echo $sel_user_row['email_address']; ?>" />
+         <input id="SaveButton" type="submit" name="submit" Value="Submit"  />
+         <input id="userid" name="userid" type="hidden" value="1001" />
+         <br /><br />
+         <div id="Status" >
+            <?php if(isset($_REQUEST['submitted'])) {
+               echo "<span class='red'>User successfully edited</span>";
+            } ?>
+         </div>
+      </div>
+   <?php } ?>    
+</form>
+<?php require_once('../includes/footer-site.php'); ?>
