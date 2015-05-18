e45c1dd39a91758563cf6f7c7e635f2f121b24e7
chrisull <cullman@velti.com>
2015-05-18 16:58:33 +0100

    Corrected article page

--- code/chapter_07/add_article_short.php
+++ code/chapter_07/add_article_short.php
@@ -9,7 +9,6 @@ if (isset($_POST['Submit'])) {
      $ins_article_set->bindParam(":category_id", $_POST['CategoryId']);
      $ins_article_set = $dbHost->prepare("INSERT INTO article (title, content, date_posted, category_id, user_id) VALUES (:article_title, :article_content, :date , :category_id)");
      $ins_article_set->execute();
-     $newarticleid = $dbHost->lastInsertId();
     if($ins_article_set->errorCode()!= 0) { 
         die("Insert Article Query failed ");
     } else {
@@ -23,10 +22,10 @@ if (isset($_POST['Submit'])) {
 } ?> 
     <h2>Add an Article</h2><br />
     <form id="galleryform" method="post" action="add-article.php" enctype="multipart/form-data">
-            <label>Title: <input id="ArticleTitle" name="ArticleTitle" type="text" /></label>
-		    <label>Content: <textarea></textarea></label>     
+            <label>Title: <input name="ArticleTitle" type="text" /></label>
+		    <label>Content: <textarea name="ArticleContent"></textarea></label>     
             <label>Category: 
-                <select id="CategoryId" name="CategoryId">
+                <select name="CategoryId">
                 <?php while($sel_category_row = $sel_cat_set->fetch()) { ?>
                     <option value="<?php  echo $sel_category_row['category_id']; ?>"><?php  echo $sel_category_row['category_name']; ?></option>
                 <?php } ?> 
@@ -35,7 +34,7 @@ if (isset($_POST['Submit'])) {
             <button name="submit" value="Submit" value="sent" class="btn btn-primary" style="float:left;display:block;">Submit Article</button>
     </form>
     <?php if (isset($_POST['submit'])) {
-                echo "<span class='red' style='color:red;'>Article successfully created!</span><br/>";  
+              echo "<span class='red' >Article ". $articleid."successfully created!</span><br/>";  
             }  ?>
 <?php include '../includes/footer-site.php' ?>
 
