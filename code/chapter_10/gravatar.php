e5dd56f64167bb8da2456949a51b402719046640
cullman1 <cullman@velti.com>
2015-06-15 18:20:08 +0100

    Updated examples

--- code/chapter_10/gravatar.php
+++ code/chapter_10/gravatar.php
@@ -1,44 +1,24 @@
-
 <?php include '../includes/header-register.php'; 
-
-$default = "http://test1.phpandmysqlbook.com/uploads/blank.png";
+$def = "http://test1.phpandmysqlbook.com/uploads/blank.png";
 $size = 100;
-$url = "";
-
-if (isset($_REQUEST["email"]))
-{
-	$url = get_gravatar($_REQUEST["email"], $size, $default, 'g', $false);
+if (isset($_POST["email"])) {
+  $url = get_gravatar($_POST["email"], $size, $def, 'g', 'false');
 }
-
-function get_gravatar( $email, $size, $default, $rating, $img, $atts = array() ) {
-    $url = 'http://www.gravatar.com/avatar/';
-    $url .= md5( strtolower( trim( $email ) ) );
-    $url .= "?s=$size&d=$default&r=$rating";
-    if ( $img ) {
-        $url = '<img src="' . $url . '"';
-        foreach ( $atts as $key => $val )
-            $url .= ' ' . $key . '="' . $val . '"';
-        $url .= ' />';
-    }
-    return $url;
-}
-
-
-?>
+function get_gravatar($email, $size, $def, $rating, $alt) {
+  $url = 'http://www.gravatar.com/avatar/';
+  $url .= md5( strtolower( trim( $email ) ) );
+  $url .= "?s=$size&d=$def&r=$rating&alt=$alt";
+  return $url;
+} ?>
 <form name="input_form" method="post" action="gravatar.php">
-      <div class="wholeform">
-      <div class="col-md-4"><h1>Search For a Gravatar</h1>
-<label for="email">Email:</label> 
-<input type="text" id="email" name="email" /><br /><br />
-<input type="submit" name="submit_button" value="Submit" /><br /><br />
-       </div>
-            <div>
- <?php  if (isset($_REQUEST["email"]))
-{  ?>
-<img src="<?php echo $url ?>" alt="" />
-<?php } ?>
-    </div>
-</div>
+  <div class="col-md-4"><h1>Search For a Gravatar</h1>
+    <label for="email">Email: 
+    <input type="text" name="email" placeholder="<?php if (isset($_POST["email"])) { echo $_POST["email"]; } ?>" /></label><br/><br/>
+    <input type="submit" name="submit" value="Submit" />
+    <?php if (isset($_POST["email"])) {  ?>
+      <img src="<?php echo $url ?>" style="padding-left:10px;" />
+    <?php } ?>
+  </div>
 </form>
 <?php include '../includes/footer-site.php' ?>
 
