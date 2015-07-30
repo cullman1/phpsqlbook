8cdac0d050dca8e4eccfae9a6e72252d61a37e0d
cullman1 <cullman@velti.com>
2015-07-30 16:00:46 +0100

    Latest examples

--- code/chapter_11/classes/email.php
+++ code/chapter_11/classes/email.php
@@ -1,16 +1,17 @@
 <?php class Emailer {
   const newline = "\r\n";
-  public $server,$port,$mailServer,$socket,$user,$password;
-  public $connectTimeout,$responseTimeout,$headers;
-  public $from,$to,$subject,$message,$contentType;
+ private $server,$port,$mailServer,$socket,$user,$password;
+  private $connectTimeout,$responseTimeout,$headers;
+  private $from,$to,$subject,$message,$contentType;
 
-function __construct($user,$password,$subject,$message) {
+ function __construct($user,$password,$subject,$message, $from) {
     $this->server = "94.76.218.242";
     $this->port = 8889; 
     $this->mailServer = "eastcornwallharriers.com"; 
     $this->connectTimeout = 60;
     $this->responseTimeout = 8;
     $this->to = array();
+    $this->from = $from;
     $this->headers = array();
     $this->user = $user;
     $this->password = $password;
@@ -19,18 +20,11 @@ function __construct($user,$password,$subject,$message) {
     $this->headers['MIME-Version'] = "1.0";
     $this->headers['Content-type'] = "text/html";
   }
-
-  function SentTo($address,$name = "") {
+ function SentTo($address,$name = "") {
      $this->to = array($address,$name);
   }
-  function SentFrom($address) {
-    $this->from = $address;
-  }  
-  private function SendCMD($CMD) {
-    fputs($this->skt, $CMD . self::newline);
-    return $this->GetResponse();
-  }
-  private function GetResponse() {
+
+ private function GetResponse() {
     stream_set_timeout($this->skt, $this->responseTimeout);
     $response = '';
     while (($line = fgets($this->skt, 515)) != false) {
@@ -39,24 +33,31 @@ function __construct($user,$password,$subject,$message) {
     }
     return trim($response);
   }
+
+ private function SendCMD($CMD) {
+    fputs($this->skt, $CMD . self::newline);
+    return $this->GetResponse();
+  }
+
   function Send() {
     $newLine = self::newline;
-    $this->skt = @fsockopen($this->server, $this->port, $errno, $errstr, $this->connectTimeout);
+   $this->skt = @fsockopen($this->server,$this->port,$errno,
+    $errstr,$this->connectTimeout);
     if (empty($this->skt)) {
         return "ERROR: ".$errstr;
     }    
     $this->GetResponse();
-    $this->SendCMD("EHLO {$this->mailServer}");
+   $this->SendCMD("EHLO {$this->mailServer}");
     $this->SendCMD("AUTH LOGIN");
     $this->SendCMD(base64_encode($this->user));
     $this->SendCMD(base64_encode($this->password));
-    $this->SendCMD("MAIL FROM:<{$this->from}>");
+   $this->SendCMD("MAIL FROM:<{$this->from}>");
     foreach ($this->to as $addr) {
-      $this->SendCMD("RCPT TO:<{$addr}>");
+     $this->SendCMD("RCPT TO:<{$addr}>");
     }
-    $this->SendCMD("DATA");
+   $this->SendCMD("DATA");
     if (!empty($this->contentType)) {
-        $this->headers['Content-type'] = $this->contentType;
+       $this->headers['Content-type'] = $this->contentType;
     }      
     $this->headers['From'] = $this->from;
     $this->headers['To'] = $this->to[0];   
@@ -66,9 +67,10 @@ function __construct($user,$password,$subject,$message) {
     foreach ($this->headers as $key => $val) {
         $headers .= $key . ': ' . $val . self::newline;
     }
-    $this->SendCMD("{$headers}{$newLine}{$this->message}{$newLine}.");
+    $this->SendCMD("{$headers}{$newLine}{$this->message}
+    {$newLine}.");
     $this->SendCMD("QUIT");
-    fclose($this->skt);
+   fclose($this->skt);
     return "true";
-  }
+ }
 } ?>
--- code/chapter_11/index.php
+++ code/chapter_11/index.php
@@ -5,6 +5,7 @@
     <ul>
       <li><a href="userclass.php">User class</a></li>
          <li><a href="new-user-short.php">User class - add image example</a></li>
+              <li><a href="email-form.php">Simple email example</a></li>
       <li><a href="forgotten-password-short.php">Forgotten Password Email Object example</a></li>
        <li><a href="timestamp.php">Timestamp</a></li>
     </ul>
