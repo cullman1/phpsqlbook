d3ff1db46ac9e7f0cfddd2483f7418320c7cdb5b
cullman1 <cullman@velti.com>
2015-09-22 18:19:29 +0100

    Latest

--- cmsfull/classes/templates/like_content.php
+++ cmsfull/classes/templates/like_content.php
@@ -1,6 +1,6 @@
 <div id="accordion3">
  <div class="accordion-group">
-  <form id="likeform" method="post" action="../pages/submit-like.php?user_id={{.userid}}&article_id={{.articleid}}&liked={{.likes_total}}">
+  <form id="likeform" method="post" action="pages/submit-like.php?user_id={{.userid}}&article_id={{.articleid}}&liked={{.likes_total}}">
    <div> 
     <span class="left">{{.likes_count}} users like this article</span>   
     <input name="like_button" type="submit" value='Like/Unlike this article'  />
--- cmsfull/pages/submit-like.php
+++ cmsfull/pages/submit-like.php
@@ -1,9 +1,11 @@
- <?php require('../classes/registry.php');
+<?php error_reporting(E_ALL | E_WARNING | E_NOTICE);
+ini_set('display_errors', TRUE);
+require('../classes/registry.php');
   require('../classes/configuration.php');
   $registry = Registry::instance();
-  $registry->set('conf', new Configuration());
+  $registry->set('configfile', new Configuration());
   $db = $registry->get('configfile');
- $connect="mysql:host=".$db->getServerName() .";dbname=".$db->getDatabaseName();
+  $connect="mysql:host=".$db->getServerName() .";dbname=".$db->getDatabaseName();
   $pdo = new PDO($connect, $db->getUserName(), $db->getPassword()); 
   $pdo->setAttribute(PDO::ATTR_FETCH_TABLE_NAMES, true);
   $registry->set('pdo', $pdo);
@@ -12,15 +14,15 @@
     header('Location:../login/login-user.php');
   } else {    
     if($_REQUEST['liked']=="0") {
-      $query = "INSERT INTO article_ like (user_id, article_id) VALUES (:userid, :articleid)";
+      $query = "INSERT INTO article_like (user_id, article_id) VALUES (:userid, :articleid)";
     } else {
       $query = "DELETE FROM article_like WHERE user_id= :userid and article_id= :articleid";
     }
-    $statment = $pdo->prepare($query);
-    $statement->bindParam(":userid", $user_id);
-    $statement->bindParam(":articleid", $article_id);
-    $statment->execute();
-    if ($statment->errorCode()!=0) {  die("Query failed"); }
+    $statement = $pdo->prepare($query);
+    $statement->bindParam(":userid", $_REQUEST["user_id"]);
+    $statement->bindParam(":articleid", $_REQUEST["article_id"]);
+    $statement->execute();
+    if ($statement->errorCode()!=0) {  die("Query failed"); }
     $return = $_SERVER["HTTP_REFERER"];
     header('Location:'.$return);
   }  ?>
