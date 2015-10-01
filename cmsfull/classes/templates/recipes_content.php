1d81462254b9b9db8de1e172b4d763de7e5814a4
cullman1 <cullman@velti.com>
2015-10-01 18:04:35 +0100

    Corrected link

--- cmsfull/classes/templates/recipes_content.php
+++ cmsfull/classes/templates/recipes_content.php
@@ -1,5 +1,5 @@
 <div class="{{category.category_template}}">
- <h3><a href="{{article.article_id}}">{{article.title}}</a>   
+ <h3><a href="/cmsfull/recipes/{{article.article_id}}">{{article.title}}</a>   
  </h3>
  <h5>{{article.date_posted}}</h5>
  <div class="box">{{article.content}}</div>
