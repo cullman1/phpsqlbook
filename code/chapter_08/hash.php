d911eb39d7bcee3a924458e00bd5ae2123c57ccf
cullman1 <cullman@velti.com>
2015-11-02 18:23:38 +0000

    Latest

--- code/chapter_08/hash.php
+++ code/chapter_08/hash.php
@@ -1,28 +1,26 @@
 <?php include '../includes/header-register.php' ?>
-<form name="input_form" method="post" action="hash.php">
-      <div class="wholeform">
-      <div class="col-md-4"><h1>Hash</h1>
-<label for="full_name">Enter a Password:</label> 
-<input type="text" id="password" name="password" /><br />
-<input type="submit" name="submit_button" value="Submit" />
-       </div>
-            <div>
-        <?php
-        if (isset($_REQUEST["password"]))
-        {
-            $sha1Password = sha1($_REQUEST["password"]);
-            $md5Password = md5($_REQUEST["password"]);
-            $preSalt = "grF!y1";
-            $afterSalt = "d!@hb3";
-            $saltedPassword = sha1($preSalt . $_REQUEST["password"] .$afterSalt);
-            echo "Your password was: ".$_REQUEST["password"]."<br/>";
-            echo "The SHA1 encrypted version was: ".$sha1Password."<br/>";
-            echo "The MD5 encrypted version was: ".$md5Password."<br/>";
-            echo "The salted version was: ".$saltedPassword."<br/>";
-        }
-        ?>
-    </div>
-</div>
+<?php 
+function hash_password($password) {
+  $sha1Passwd = sha1($password);
+  $md5Passwd = md5($password);
+  $preSalt = "grF!y1";
+  $afterSalt = "d!@hb3";
+  $saltedPasswd=sha1($preSalt.$password .$afterSalt);            
+  echo "Your password was: ".$password."<br/>";  
+  echo "The SHA1 encrypted version was:".$sha1Passwd."<br/>";
+  echo "The MD5 encrypted version was: ".$md5Passwd."<br/>";
+  echo "The salted version was: ".$saltedPasswd."<br/>";  
+} ?>
+<form name="input_form" method="post" class="indent" style="width:470px;" action="hash.php">
+  <h1>Hash</h1>
+  <label for="full_name">Enter a Password:</label> 
+  <input type="text" id="password" name="password" />
+  <div class="clear"></div>
+  <input type="submit" value="Submit" />
+  <div class="clear"></div>
+  <?php if (isset($_POST["password"])) {
+    hash_password($_POST["password"]);
+    } ?>
 </form>
 
 
