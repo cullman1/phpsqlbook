183c2cf6cd765df52d53a057f683dc9b88905df0
chrisull <cullman@velti.com>
2016-03-23 16:56:47 +0000

    Commit images

---
+++ code/chapter_08/upload-file-save-data-to-database.php
@@ -0,0 +1,73 @@
+<?php 
+  require_once('includes/database-connnection.php'); 
+
+  function clean($content) {
+    $content = trim($content);
+    $content = htmlspecialchars($content);
+    return $content;
+  }
+
+  function get_image_upload_form(){
+    $form = '<form method="post" action="" enctype="multipart/form-data">
+      <label>Title: <input type="text" name="title" /></label><br>
+      <label>Alt text: <input type="text" name="alt" /></label><br>
+      <label>Image: <input type="file" name="image" accept="image/*" /></label><br>
+      <button type="submit" name="submitted" value="sent">Submit</button>
+    </form>';
+    return $form;
+  }
+
+  function upload_file($file, $title, $alt) {
+    $date      = date("Y-m-d H:i:s");        // Today's date
+    $type      = $file['type'];     // Type of file from $_FILES superglobal
+    $temporary = $file['tmp_name']; // Temp file location $_FILES superglobal
+    $filename  = $file['name'];     // Name of file from $_FILES superglobal
+    $filepath  = '../uploads/' . $filename;     // Filepath = relative directory + filename
+    $thumb     = '';                         // Will be added on pXXX
+
+    if(move_uploaded_file($temporary, $filepath)) {  // Try to move file, if it works
+      $sql = "INSERT INTO media(title, alt, date, type, filename, filepath, thumb) 
+      VALUES (:title,:alt,:date,:type,:filename,:filepath,:thumb)";  // Create SQL
+      $statement = $GLOBALS['connection']->prepare($sql);            // Connect
+      $statement->bindParam(":title",    $title);                    // Bind parameters
+      $statement->bindParam(":alt",      $alt);
+      $statement->bindParam(":date",     $date);
+      $statement->bindParam(":type",     $type);
+      $statement->bindParam(":filename", $filename);
+      $statement->bindParam(":filepath", $filepath);
+      $statement->bindParam(":thumb",    $thumb);
+      $statement->execute();                                       // Execute
+      if($statement->errorCode()==0) {                             // If no errors
+        return $filename . ' uploaded successfully';               // Return success msg
+      } else {                                                     // Otherwise
+        return 'Information about your file could not be saved.';  // Say info not saved
+      }
+    } else {                                                        // Couldn't move file
+      return 'Your image could not be saved.';                      // Say image not saved
+    }
+  }
+
+  if( isset($_FILES['image']) ) {    // If file present get title and alt text
+      $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS); // Get file title
+      $alt   = filter_input(INPUT_POST, 'alt', FILTER_SANITIZE_SPECIAL_CHARS);   // Get alt text
+  }
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Upload file then move</title>
+  </head>
+  <body>
+
+    <?php
+    if ( empty($title) || empty($alt) ) {
+      echo get_image_upload_form();
+    } else { 
+      echo upload_file($_FILES['image'], $title, $alt);
+    }
+    ?>
+
+  </body>
+</html>
