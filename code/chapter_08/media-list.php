1f92c9cfbea8e381946cfe7872cfb27d509ff4ea
chrisull <cullman@velti.com>
2016-04-13 15:27:02 +0100

    Latest

--- code/chapter_08/media-list.php
+++ code/chapter_08/media-list.php
@@ -5,18 +5,19 @@ $message = '';
 $orderby   = ( isset($_GET['orderby'])   ? $_GET['orderby']    : '' );
 $direction = ( isset($_GET['direction']) ? $_GET['direction']  : '' );
 include 'includes/header.php';
-
+$media_list = get_media_list($orderby, $direction);
 ?>
 
 <div class="container" role="main">
-  <?php $media_list = get_media_list($orderby, $direction);
-  foreach ($media_list as $media) {                          // Loop through media ?> 
-  <div class="col-md-3" id="thumbnails">
-    <div class="panel panel-default">
-      <div class="panel-heading"><?=$media->title?></div>
-      <div class="panel-body"><?= display_media($media); ?></div>
-    </div>
-  </div>
-<?php } ?>
+  <div class="page-header"><h2>media</h2></div>
+<table class='table table-striped'>
+  <?php foreach ($media_list as $media) { ?>
+  <tr>
+    <td><?=$media->title;?></td>
+    <td><?=display_media($media);?></td>
+    <td><?=$media->type;?></td>
+  </tr>
+    <?php } ?>
+</table>
 </div><!-- .container -->
 <?php include 'includes/footer.php'; ?>
