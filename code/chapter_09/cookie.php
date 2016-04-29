0e24ea54351d837c376d45c83e8f1702ea2a718b
chrisull <cullman@velti.com>
2016-04-29 17:15:16 +0100

    Updated

--- code/chapter_09/cookie.php
+++ code/chapter_09/cookie.php
@@ -1,11 +1,10 @@
 <?php 
- check_cookie($_COOKIE["UserName"], $_POST["name"], $_POST["remember_me"]);
-  
   function check_cookie($cookie, $name, $rememberme) {  
-  if (isset($cookie)) {
-      echo "Hello ".$cookie.", welcome back to our site";
+    if (isset($cookie)) {
+      return 'Hello '.$cookie.', welcome back to our site';
     } else {
-      set_cookie($name, $rememberme);
+      $message = set_cookie($name, $rememberme);
+      return $message;
     } 
   }
   
@@ -14,16 +13,21 @@
        setcookie("UserName",$name,time()+604800,'/'); 
     }   
    if (!empty($name)) { 
-      echo "Hello ".$name.", welcome to our site";
-    }
+      return "Hello ".$name.", welcome to our site";
+    } else {
+      return '';
+}
   } 
+
+echo check_cookie($_COOKIE["UserName"], $_POST["name"], $_POST["remember_me"]); 
 ?>
+
 <form name="input_form" method="post" action="cookie.php">
   <?php if (!isset($_POST["name"]) && !isset($_COOKIE["UserName"]) ) { ?> 
-    <label for="name">Full Name:</label> 
+    <label for="name">Name:</label> 
     <input type="text" name="name" /><br />
     <label for="remember_me">Remember Me?</label> 
     <input type="checkbox" name="remember_me" value="remember"/>
     <br/><input type="submit" name="submit" value="Submit"/>
   <?php } ?> 
-</form>   
+</form> 
