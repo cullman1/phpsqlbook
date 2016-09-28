9ff66b8e13f646c147a23d26ef99e9dd5ec5b801
chrisull <cullman@velti.com>
2016-09-28 16:35:12 +0100

    Latest

--- code/chapter_03/testing-access.php
+++ code/chapter_03/testing-access.php
@@ -2,14 +2,16 @@
 error_reporting(E_ALL | E_WARNING | E_NOTICE);
 ini_set('display_errors', TRUE);
 
+
+
 class Vehicle {
 
   private $make  = 'porsche'; 
- 
-  private $year  = 1992;
+   
+  static $year  = 1992;
 }
 
 $car = new Vehicle();
-
+echo $car::$year;
 echo $car->year;
 ?>
