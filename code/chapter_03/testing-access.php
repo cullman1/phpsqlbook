0da96503db7728f0541e93c5453ccc29ddec8e13
cullman1 <cullman@velti.com>
2016-09-29 18:03:37 +0100

    Latest

--- code/chapter_03/testing-access.php
+++ code/chapter_03/testing-access.php
@@ -2,16 +2,24 @@
 error_reporting(E_ALL | E_WARNING | E_NOTICE);
 ini_set('display_errors', TRUE);
 
-
-
-class Vehicle {
-
-  private $make  = 'porsche'; 
-   
-  static $year  = 1992;
+class Seed {
+  public  $name  = 'Basil';
+  private $stock = 130;
+  public static  $tax_rate  = 18;
+  
+  public static function getPrice($gross) {
+    $tax= ($gross/100) * 18;
+    return $tax;
+  }
+  
+  public function getStockLevel() {
+    return $this->stock;
+  }
 }
 
-$car = new Vehicle();
-echo $car::$year;
-echo $car->year;
+echo  Seed::$tax_rate . '%' . '<br>'; //Static property
+echo  Seed::getPrice(2000) . '<br>';  //Static method
+$seed = new Seed();
+echo  $seed->name . '<br>';           //Public property
+echo  $seed->getStockLevel() . '<br>';//Private method
 ?>
