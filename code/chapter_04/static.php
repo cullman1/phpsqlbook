57a8c487cf22d659569c972a9fac5c7b02b3fd87
cullman1 <cullman@velti.com>
2017-02-17 18:52:01 +0000

    Latest

--- code/chapter_04/static.php
+++ code/chapter_04/static.php
@@ -9,15 +9,18 @@ class Calculator {
     return $digit1 - $digit2;
   }
 
-  public static function static_subtract($digit1, $digit2)  
+  public static function static_subtract($digit1, 
+  $digit2)  
   {
     return $digit1 - $digit2;
   }
 }
 
-echo "STATIC". Calculator::static_subtract(10, 1);
-echo "NON STATIC". Calculator::subtract(10, 1);
+echo "Before Object instantiation static calls";
+echo "<br/>STATIC ". Calculator::static_subtract(10,1);
+echo "<br/>NON STATIC ". Calculator::subtract(10,1);
+echo "<br/>After Object instantiation non-static calls";
 $calculator = new Calculator();
-echo "NON STATIC". $calculator->subtract(10,1);
-echo "STATIC". $calculator->static_subtract(10,1);
+echo "<br/>NON STATIC". $calculator->subtract(10,1);
+echo "<br/>STATIC". $calculator->static_subtract(10,1);
 ?>
