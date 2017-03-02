da00822714ee1e64329dbc22056de2837f11a3b5
chrisull <cullman@velti.com>
2017-03-02 14:12:30 +0000

    Latest

--- code/chapter_04/class_lib.php
+++ code/chapter_04/class_lib.php
@@ -24,26 +24,23 @@ class User {
 
 
 
-class Account {   
- public $name;
-  public $number;
-  private $balance;
-  public static $rate;
-  public static $high;
+class Account {
+    public $name;
+    public $number;
+    private $balance;
+    public static $rate;
 
-  function __construct($name, $number, $balance) { 
-    $this->name = $name;
-    $this->number = $number;
-    $this->balance = $balance; 
-  }
-  public static function getHigh($balance) {
-    return $balance * self::$high;
-  }
-  public function getInterest() {
-    return $this->balance * self::$rate;
-  }
-
+    function __construct($name, $number, $balance) { 
+        $this->name = $name;
+        $this->number = $number;
+        $this->balance = $balance; 
+    }
+    
+    public static function getInterest($sum) {
+        return $sum * self::$rate;
+    }
 }
+
 class SavingsAccount extends Account {
   public static $fee;
 
