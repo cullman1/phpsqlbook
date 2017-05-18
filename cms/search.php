73def6203bd1c19e18decdad4ceffc6518be44ef
chrisull <cullman@velti.com>
2017-05-18 17:04:11 +0100

    latest

--- cms/search.php
+++ cms/search.php
@@ -3,13 +3,14 @@ session_start();
 require_once('includes/class_lib.php');
 require_once('includes/functions.php');
 $GLOBALS['root'] = "/phpsqlbook/cms/";
-$term = ( isset($_GET['term']) ? $_GET['term'] : 'avc' ); 
-var_dump($_REQUEST);
-echo "term".$term;
+$term = ( isset($_GET['term']) ? $_GET['term'] : '' ); 
 getHTMLTemplate('header');
 $articlelist = get_articles_by_search(5,0,'','',$term,'');
 foreach($articlelist as $object) {
-   // getHTMLTemplate('main_content',$object);  
+    getHTMLTemplate('main_content',$object);  
+}
+if (empty($articlelist)) {
+    echo '<span>No articles containing the term "' . $term . '" were found';
 }
 getHTMLTemplate('footer');
 ?>
